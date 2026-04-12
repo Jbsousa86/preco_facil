b67a8eedd37833be00a812627509886044397d36
name <97321565+Jbsousa86@users.noreply.github.com>
2026-04-12 11:40:43 -0300

    fix: garantindo static serving de arquivos html mesmo em ambiente Vercel para evitar 404

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
-// Só serve estático pelo Express se NÃO estiver no Vercel
-if (!process.env.VERCEL) {
-    app.use(express.static(__dirname));
-}
+// Serve arquivos estáticos (HTML, CSS, JS)
+app.use(express.static(__dirname));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // Configuração do Multer para Uploads
